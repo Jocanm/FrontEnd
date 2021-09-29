3e377c4dacce00e51f5bd22e68ecf73697e688bf
adriana75 <tecnologianormal@hotmail.com>
2021-09-28 19:49:38 -0500

    Se agrega título al escritorio

--- ventas/src/components/Escritorio.js
+++ ventas/src/components/Escritorio.js
@@ -5,15 +5,16 @@ const Escritorio = () => {
     return (
         <div>
             <br></br><br></br><br></br><br></br>
+            <h1>SISTEMA ADMINISTRADOR DE VENTAS</h1>
             <br></br><br></br><br></br><br></br>
             <ul class="menu">
                 <li><button class="button" type="submit" name="gproductos"><h3>GESTIÓN DE PRODUCTOS</h3></button></li>
-                <li><Link to="/ListarVentas">
+                <li>
+                <Link to="/ListarVentas">
                     <button class="button" type="submit" name="gventa"><h3>GESTIÓN DE VENTAS</h3></button>
                 </Link></li>
                 <li><button class="button" type="submit" name="gusuarios"><h3>GESTIÓN DE USUARIOS</h3></button></li>
             </ul>
-            <br></br><br></br><br></br><br></br>
             <br></br><br></br><br></br><br></br>
         </div>
     )
