697bd448c11d229a0de852cadb39d4e1c0153f14
Jose Angarita <amjose09@gmail.com>
2021-09-30 11:46:04 -0500

    tailwind Instalado

---
+++ ventas/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../images/logo.jpg    '
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={logo} alt="Logo" />
+        </nav>
+    )
+}
+
+export default Navbar
