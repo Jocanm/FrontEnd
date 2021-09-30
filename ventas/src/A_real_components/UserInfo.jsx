fdd3de0df91e5ddd3923d6fdb097bef57ffec2fd
Jose Angarita <amjose09@gmail.com>
2021-09-30 11:21:56 -0500

    0 changes ListarUsuarios

--- ventas/src/A_real_components/UserInfo.jsx
+++ ventas/src/A_real_components/UserInfo.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
-import ActualizarUsuario from '../components/ActualizarUsuario';
+import ActualizarUsuario from './ActualizarUsuario';
 
 const UserInfo = ({ usuarios }) => {
 
