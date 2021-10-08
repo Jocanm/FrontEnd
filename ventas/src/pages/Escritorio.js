7bfac0b7c882d0b3e991dcb56f7d4f605e2059cb
Jose Angarita <amjose09@gmail.com>
2021-10-08 08:00:43 -0500

    responsive escritorio

--- ventas/src/pages/Escritorio.js
+++ ventas/src/pages/Escritorio.js
@@ -3,20 +3,20 @@ import {Link} from "react-router-dom"
 
 const Escritorio = () => {
     return (
-        <div className="mt-28">
+        <div className="mt-20">
             <h1 className="font-bold text-2xl">SISTEMA ADMINISTRADOR DE VENTAS</h1>
-            <ul className="flex justify-around items-center mt-10">
-                <li>
+            <ul className="flex flex-col xl:flex-row justify-around items-center mt-10 rounded">
+                <li className="mb-4">
                 <Link to='/ListarProductos'>
                     <button className="button px-10 font-bold" type="submit" name="gproductos"><h3>GESTIÓN DE PRODUCTOS</h3></button>
                 </Link>
                 </li>
-                <li>
+                <li className="mb-4">
                 <Link to="/ListarVentas">
                     <button className="button px-10 font-bold" type="submit" name="gventa"><h3>GESTIÓN DE VENTAS</h3></button>
                 </Link>
                 </li>
-                <li>
+                <li className="mb-4">
                 <Link to='/Usuarios'>
                     <button className="button px-10 font-bold" type="submit" name="gusuarios"><h3>GESTIÓN DE USUARIOS</h3></button>
                 </Link>    
